51cfb4d57015b43674da10e54b2b246a56d2af36
小乐老板 <1124842985@qq.com>
2025-11-09 09:29:50 +0800

    测试

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello, World！！！！！！！！！！!\n");
     printf("this is a git ");
     printf("demo project.\n");
     return 0;
